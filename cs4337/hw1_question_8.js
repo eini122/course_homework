047d66ab10c85a0a7aec6e7fdb5d9901725ffb7a
peter <likaitian731@gmail.com>
2019-09-13 23:30:43 -0500

    update

--- cs4337/hw1_question_8.js
+++ cs4337/hw1_question_8.js
@@ -1,2 +1,19 @@
-var color_input = document.getElementById("input").VALUE;
-if(color_input == "red") alert("red");
+// var color_input = document.getElementById("input");
+// if(color_input == "red" ) alert("red");
+
+function validateForm() {
+    var color = document.forms["myForm"]["text"].value;
+    if (color == "red") {
+        document.body.style.backgroundColor = "red";
+    }
+    else if(color == "blue"){
+        document.body.style.backgroundColor = "blue";
+    }
+    else if(color == "yellow"){
+        document.body.style.backgroundColor = "yellow";
+    }
+    else{
+        alert("Please enter a valid color. (red, yellow, blue)");
+    }
+    return false;
+  }
